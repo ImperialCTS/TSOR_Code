e5e1585cd4ff2c746ba35a66974fd4e5d941b9cd
Panagiotis Angeloudis <p@ngl.gr>
2017-11-26 21:39:16 +0000

    new version of the class GA example

--- GA_Code/class_example.m
+++ GA_Code/class_example.m
@@ -2,61 +2,74 @@
 % generic genetic algorithm with different stages separated into their own
 % methods - stand-alone version
 
-function [] = GeneticAlgorithm()
-% Main method - calls the rest of the function in the following order:
-% generate initial solution, calulate fitness, crossover, mutate, and
-% feasibility stage, then repeat until generations have run out.
-% Needs to be given the problem inputs
+clear;
 
 % initialises problem parameter
-paramsGA = InitialiseParameters(); 
+params = InitialiseParameters(); 
 
 % initialise variables to save best solutions
-bestPerGen.sol = cell(paramsGA.population,1);
-bestPerGen.fit = zeros(paramsGA.population,1);
+history.sol = cell(params.population,1);
+history.fit = zeros(params.population,1);
+
 best.sol = cell(1,1);
 best.fit = 0;
 
+%j = figure;
+%hold on;
+%title('Fitness Value at ');
+%xlabel('Generations');
+%ylabel('Fitness Value');
+%xlim([0, params.generations]);
+%hold off;
+
 % generate initial population
-parentPop = Generation(paramsGA);
+populationCurrent = GeneratePopulation(params);
 
 % repeat for all specified
-for i = 1 : paramsGA.generations
+for i = 1 : params.generations
     
-    fitness = ObjectiveFunction(parentPop, paramsGA);
-    [best, bestPerGen] = SaveBestSolution(parentPop, fitness, best,bestPerGen, i);
-    newPop = Selection(parentPop, fitness, paramsGA.population);
-    childPop = Crossover(newPop, paramsGA);
-    childPop = Mutation(childPop, paramsGA);
+    fitness = ObjectiveFunction(populationCurrent, params);
+          
+    [best, history] = SaveBestSolution(populationCurrent, fitness, best, history, i);
     
-    parentPop = childPop;
+    populationNew = Selection(populationCurrent, fitness, params);
+    populationNew = Crossover(populationNew, params);
+    populationNew = Mutation(populationNew, params);
+    
+    populationCurrent = populationNew;
 end
 
+best_x1 = best.sol{1}(1);
+best_x2 = best.sol{1}(2);
+best_x3 = best.sol{1}(3);
+best_x4 = best.sol{1}(4);
 
-end
+bestFitness = best.fit;
 
-%% GENETIC ALGORITHM INITIALISATION
+%end
+
+%%  ------------------- INITIALISATION
 
 function [p] = InitialiseParameters()
 % initialises parameters for the genetic algorithm to function. Uses a
 % struct mechanism, in which all variables are stored into a main struct.
 
 p.population = 200;                 % size of solution population
-p.chromNum = 2;                     % size of inidividual solution
-p.generations = 1000;               % number of generations
+p.genes = 4;                       % size of inidividual solution
+p.generations = 5000;              % number of generations
 
-p.childNum = p.population;          % number of children produced directly 
-                                    % from parents
+p.elitesize = p.population/5;       % number of children produced directly 
+                                   % from parents
 p.upBound = 0;                     % upper bound for number generator
-p.lowBound = 5000;                   % lower bound for number generator
-p.crossoverProb = 0.7;              % crossover probability
-p.mutationProb = 0.1;               % mutation probability
+p.lowBound = 100;                 % lower bound for number generator
+p.crossoverProb = 0.7;             % crossover probability
+p.mutationProb = 0.2;              % mutation probability
 
 end
 
-%% GENERATION
+%% ------------------- GENERATION
 
-function [pop] = Generation(params)
+function [pop] = GeneratePopulation(params)
 % generates randomised population of solution - requires population size
 % (popSize) and chromosome size (chromSize) as inputs. The method assumes 
 % the chromosome is 1 dimensional.
@@ -65,20 +78,19 @@ popNum = params.population;
 pop = cell(popNum, 1);
 
 for i = 1 : popNum
-    pop{i} = GenerateChromosome(params.chromNum, params);
+    pop{i} = GenerateChromosome(params.genes, params);
     
 end
 
 end
 
-function [gene] = GenerateChromosome(chromSize, params)
+function [gene] = GenerateChromosome(geneCount, params)
 
-gene = params.lowBound + rand(chromSize,1) * ...
-        (params.upBound - params.lowBound);
+gene = params.lowBound + rand(geneCount,1) * (params.upBound - params.lowBound);
 
 end
 
-%% EVALUATION OF SOLUTIONS & SAVING BEST SOLUTIONS
+%%  ------------------- EVALUATION
 
 function [fit] = ObjectiveFunction(pop, params)
 % calculates the "goodness" of the solution. In this case, a value is added
@@ -90,16 +102,18 @@ for i = 1 : params.population
     thisChromo = pop{i};
     x1 = thisChromo(1);
     x2 = thisChromo(2);
+    x3 = thisChromo(3);
+    x4 = thisChromo(4);
 
-    fit(i) = 2* x1 + 4*x2;
+    fit(i) = 4*x1^5+2*x2*log(x3)+x4;
     
-    if ~(3*x1+4*x2<=1700)
-        fit(i) = -Inf;
+    if ~(x1+x2<=40)
+        fit(i) = 0;
     end 
     
-    if ~(2*x1+5*x2<=1600)
-        fit(i) = -Inf;
-    end 
+    if ~(x3+x4<=30)
+       fit(i) =0;
+   end 
     
 end
 
@@ -113,8 +127,8 @@ function [best, bestGen] = SaveBestSolution(pop, fit, best, bestGen, gen)
 
 % Find solution with lowest fitness in the generation
 popSize = length(pop);
-fitness = 0;
-bestInGen 
+fitness = -Inf;
+
 for i = 1 : popSize
     if (fitness <= fit(i))
         fitness = fit(i);
@@ -133,9 +147,9 @@ end
 
 end
 
-%% SELECTION METHOD
+%%  ------------------- SELECTION METHOD
 
-function [newPop] = Selection(pop, fit, popSize)
+function [new_population] = Selection(population, pop_fitnesses, params)
 % extracts solutions to be carried over for manipulation. Uses a roullete
 % method approach. Calculates cumulative fitness of the solutions, then a
 % random number is assigned and the solution that corresponds to this
@@ -143,58 +157,45 @@ function [newPop] = Selection(pop, fit, popSize)
 % being extracted. Inputs required are the population (pop) and the fitness
 % (fit).
 
-lastFit = 0;
-minFit = min(fit);
-fit = fit - minFit; % make the worst solution = 0, for readability
-newPop = pop;
-totFit = sum(fit);
-cumulativeFit = zeros(length(pop),1);
+%sort the population
+[sorted_fitness, sort_sequence] = sort(pop_fitnesses, 'descend');
+sorted_population = population(sort_sequence,:);
 
-% calculate cumulative fitness values for solutions. i.e. given two
-% solutions of fitness 2 and 3 respectively, the resultant cumulative
-% fitness will be 2/5 and 5/5.
-for i = 1 : popSize
-    cumulativeFit(i) = fit(i)/totFit + lastFit;
-    lastFit = cumulativeFit(i);
+new_population = sorted_population;
+
+for i = 1 : params.elitesize
+ new_population(i) = sorted_population(i);
+
 end
 
-% following the previous example, if the random number "diceRoll" is
-% smaller than 2/5, the first solution is selected, if greater than 2/5 and
-% smaller or equal to 1, then second solution is selected.
-for i = 1 : popSize
-    diceRoll = rand;
-    for j = 1 : popSize
-        if (diceRoll <= cumulativeFit(j))
-            newPop(i) = pop(j);
-            break;
-        end
-    end
+for i = params.elitesize : params.population
+    new_population{i} = GenerateChromosome(params.genes, params);
 end
 end
 
-%% CROSSOVER FUNCTION
+%%  ------------------- CROSSOVER FUNCTION
 
-function [pop] = Crossover(pop, params)
-% selects another population at random and carries out a uniform crossover.
+function [population] = Crossover(population, params)
+% selects another chromosome at random and carries out a uniform crossover.
 % Each gene has a 50% probability of coming from the mother or the father.
 % Two children are created, each having opposite genes (ie. if one has a
 % gene from mother, the other has a gene from father). Inputs are
 % parent population (pop) and genetic parameters (params).
 
-for i = 1 : params.population
+for i = params.elitesize : params.population
     if (rand > params.crossoverProb)
         continue;
     end
     
 %     grab initial solution to manipulate
-    father = pop{i};
+    father = population{i};
     
-%     find second solutoin to carry out crossover with
+%     find second solution to carry out crossover with
     indexMother = i;
     while indexMother == i
         indexMother = randi(params.population);
     end
-    mother = pop{indexMother};
+    mother = population{indexMother};
     
 %     create offsprings
     son = father;
@@ -202,7 +203,7 @@ for i = 1 : params.population
     
 %     at every gene, there is a probability of 50% that the gene comes from
 %     mother or the father
-    for j = 1 : params.chromNum
+    for j = 1 : params.genes
         
 %         since "son" is created from "father" and "daughter" is created
 %         from "mother", skip step if change is not necessary.
@@ -214,27 +215,27 @@ for i = 1 : params.population
         daug(j) = son(j);
     end
 %     update both solutions in main population
-    pop{i} = son;
-    pop{indexMother} = daug;
+    population{i} = son;
+    population{indexMother} = daug;
 end
  
 end
 
-%% MUTATION FUNCTION
+%%  ------------------- MUTATION FUNCTION
 
 function [pop] = Mutation(pop, params)
 % carries out uniform mutation. A random number of genes and random values
 % are generated and the population changed. Inputs are population solution
 % (pop) and GA parameters (params).
-for i = 1 : params.population
+for i = params.elitesize : params.population
 %     if random value is greater than the probability, skip population
     if (rand > params.mutationProb)
         continue;
     end
     
 %     find gene to mutate
-    geneMutationNum = randi(params.chromNum);
-    geneToMutate = randi(params.chromNum, 1, geneMutationNum);
+    geneMutationNum = randi(params.genes);
+    geneToMutate = randi(params.genes, 1, geneMutationNum);
 %     generate new value at gene
     geneValues = GenerateChromosome(geneMutationNum, params);
     
@@ -243,5 +244,20 @@ for i = 1 : params.population
     popToMutate(geneToMutate) = geneValues;
     pop{i} = popToMutate;
 end
+
+end
+
+
+
+function [plot2] = UpdateGraphs(bests, gen, fig2)
+
+set(0,'CurrentFigure',fig2)
+hold on
+
+plot2{1,1} = plot(1:gen,bests.fit(1:gen),'r', 'LineWidth', 2); 
+fitness = bests.fit(gen);
+title(strcat('Fitness Value = ', num2str(fitness)));
+hold off
+drawnow;
 
 end
